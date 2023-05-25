8db1439b296447e1646b192a10795112c221fe67
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-05-25 12:42:01 -0700

    HTML and PHP
    Stopped on Variables

---
+++ tutorial/freeCodeCamp/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+//Use 'php -S localhost:5000' on Terminal in this directory.
+//This will run php files.
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+
+    <body>
+        <?php
+            echo("Hello World!");
+        ?>
+    </body>
+</html>
