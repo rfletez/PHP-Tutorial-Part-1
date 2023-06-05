fe4e4ca537ac1b0a602c7dc3f7b54b52088bfa81
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-06-05 16:07:06 -0700

    Open page

--- tutorial/freeCodeCamp/HelloWorld.php
+++ tutorial/freeCodeCamp/HelloWorld.php
@@ -3,21 +3,24 @@
 //This will run php files.
 ?>
 
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-    </head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-    <body>
-        <?php
-            echo("Hello World!!! <br>");
+<body>
+    <?php
+        echo("Hello World!!! <br>");
 
-            $characterName="John";
-            $characterAge=60;
+        $characterName="John";
+        $characterAge=60;
 
-            echo("There was a man named $characterName, 
-                and he was $characterAge years old.");
-        ?>
-    </body>
+        echo("There was a man named $characterName, 
+            and he was $characterAge years old.");
+    ?>
+</body>
 </html>
