b76fd1c9fa2c6cd09aa9d22e65adf956c100575f
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-06-06 12:09:07 -0700

    Get user inputs

---
+++ tutorial/freeCodeCamp/UserInput.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Input</title>
+</head>
+
+<body>
+
+    <form action="UserInput.php" method="get">
+        Name: <input type="text" name="username"/>
+        <br>
+        Age: <input type="number" name="userage"/>
+        <button type="submit">Submit</button>
+    </form>
+    <br>
+
+    Your name is <?php echo($_GET["username"]); ?>
+    and your age is <?php echo($_GET["userage"]); ?>
+</body>
+</html>
