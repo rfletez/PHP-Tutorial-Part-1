8229e59b42acf679e20d60c979a3f530f5bf0faf
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-06-06 12:37:54 -0700

    Working on a simple calculator

--- tutorial/freeCodeCamp/UserInput.php
+++ tutorial/freeCodeCamp/UserInput.php
@@ -19,5 +19,20 @@
 
     Your name is <?php echo($_GET["username"]); ?>
     and your age is <?php echo($_GET["userage"]); ?>
+
+
+    <h3>Building Calculator</h3>
+    <form action="UserInput.php" method="get">
+        Num1: <input type="number" name="num1" />
+        <br>
+        Num2: <input type="number" name="num2" />
+        <br>
+
+        <input type="submit" />
+    </form>
+
+    Answer: <?php echo($_GET["num1"] + $_GET["num2"]); 
+    //Check the URL after submitting.
+    ?>
 </body>
 </html>
