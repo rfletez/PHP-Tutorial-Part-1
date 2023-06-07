421f75d7c37070b0090cc927611aa9cdc6f44ba8
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-06-07 16:11:55 -0700

    Include PHP Files
    Including PHP files can assign variables from the included file, and retrieve functions or variables from the same file.

---
+++ tutorial/freeCodeCamp/article-header.php
@@ -0,0 +1,12 @@
+<h2> <?php echo($title); ?> </h2>
+
+<h4> <?php echo($author); ?> </h4>
+
+word count: <?php echo($wordCount); ?>
+
+<?php
+    $feetInMile = 5280;
+    function sayHi($name) {
+        echo("Hi $name.");
+    }
+?>
