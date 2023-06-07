8891376954feaf2ba825e70f3c9e5ac76a43520a
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-06-07 15:27:21 -0700

    Looping examples

--- tutorial/freeCodeCamp/Looping.php
+++ tutorial/freeCodeCamp/Looping.php
@@ -8,8 +8,36 @@
 </head>
 
 <body>
+    <form action="Looping.php" method="post">
+        What was your grade? 
+        <input type="text" name="grade" />
+        <input type="submit" />
+    </form>
+
     <?php
-    
+        $grade = $_POST["grade"];
+
+        switch($grade) {
+            case "A":
+                echo("You did amazing!");
+                break;
+            case "B":
+                echo("You did alright.");
+                break;
+            case "C":
+                echo("You could do better next time.");
+                break;
+            case "D":
+                echo("You need to spend more time studying.");
+                break;
+            default:
+                echo("You failed this class.");
+        }
+
+        $luckyNumbers = array(1, 14, 17, 8, 25, 36, 50, 1011);
+        for($x = 0; $x < count($luckyNumbers); $x++) {
+            echo("$luckyNumbers[$x] <br>");
+        }
     ?>
     
 </body>
